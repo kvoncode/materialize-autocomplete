d797bf8c9cb187bbfa372f1b91a01a987b75930e
kvoncode <kvoncode@gmail.com>
2019-09-01 10:09:25 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("input.autocomplete").autocomplete({
+    data: {
+      Apple: null,
+      Microsoft: null,
+      Google: "https://placehold.it/250x250"
+    }
+  });
+});
