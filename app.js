14f7c954c6b0c5898121b63f37f3e7d295705c67
kvoncode <kvoncode@gmail.com>
2019-09-02 18:51:50 +0900

    add: load json

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+
+  $.getJSON("countries.json", function (data) {
+      console.log("asd");
+    }
+  );
+      console.log("asd");
+
+
   $("input.autocomplete").autocomplete({
     data: {
       Apple: null,
@@ -6,4 +14,5 @@ $(document).ready(function() {
       Google: "https://placehold.it/250x250"
     }
   });
+
 });
