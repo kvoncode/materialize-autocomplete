74789a5319c28478bd659dceb1abe3eab2193db3
kvoncode <kvoncode@gmail.com>
2019-09-04 16:44:11 +0900

    add: xmlhttp request console log

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 $(document).ready(function() {
+  // const url =
+  //   "https://raw.githubusercontent.com/kvoncode/materialize-autocomplete/master/countries.json";
+  // $.getJSON(url).done(function(data) {
+  //   console.log("TCL: data", data);
+  // }).always( () => {console.log("always")})
+  // .fail( () => { console.log("fail")});
 
-  const url =
-    "https://raw.githubusercontent.com/kvoncode/materialize-autocomplete/master/countries.json";
-  $.getJSON(url).done(
-    function (data) {
-    console.log("TCL: data", data)
-      
-    }
-  );
+  // $("input.autocomplete").autocomplete({
+  //   data: {
+  //     Apple: null,
+  //     Microsoft: null,
+  //     Google: "https://placehold.it/250x250"
+  //   }
+  // });
 
-  $("input.autocomplete").autocomplete({
-    data: {
-      Apple: null,
-      Microsoft: null,
-      Google: "https://placehold.it/250x250"
+  const xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function() {
+    console.log(
+      "TCL: xhttp.onreadystatechange -> this.readyState",
+      this.readyState
+    );
+
+    if(this.readyState == 4) {
+      this.status;
+      console.log("TCL: xhttp.onreadystatechange -> this.status", this.status)
+
+      this.responseText;
+      console.log("TCL: xhttp.onreadystatechange -> this.responseText", this.responseText)
     }
-  });
+  };
+
+  xhttp.open("GET", "countries.json", true);
+
+  xhttp.send();
 });
