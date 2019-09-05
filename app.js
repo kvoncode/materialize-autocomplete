92ad68fc9094f185dc7c2d8875cb7449bc4371c4
kvoncode <kvoncode@gmail.com>
2019-09-05 10:08:21 +0900

    add: tutorial

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 $(document).ready(function() {
-  $("input.autocomplete").autocomplete({
-    data: {
-      Apple: null,
-      Microsoft: null,
-      Google: "https://placehold.it/250x250"
+  function bindData(data) {
+    $("input.autocomplete").autocomplete({
+      data: data
+    });
+  }
+
+  const url = "countries.json";
+
+  const xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function() {
+    if (4 == this.readyState && 200 == this.status) {
+      const parsedData = JSON.parse(this.responseText);
+      // console.log("TCL: xhttp.onreadystatechange -> parsedData", parsedData)
+
+      const compatibleData = {};
+
+      parsedData.forEach(element => {
+        compatibleData[element["name"]] = null;
+      });
+
+      // console.log(
+      //   "TCL: xhttp.onreadystatechange -> compatibleData",
+      //   compatibleData
+      // );
+
+      bindData(compatibleData);
     }
-  });
+  };
+
+  xhttp.open("GET", url, true);
+  xhttp.send();
 });
